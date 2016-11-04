87ee3b1bbb8b031e455123da204d695079b77a10
wassupnari <wassupnari@gmail.com>
2016-11-03 20:14:25 -0700

    Added gesture to other faces

--- CanvasLab/ViewController.swift
+++ CanvasLab/ViewController.swift
@@ -123,15 +123,111 @@ class ViewController: UIViewController {
     }
     
     @IBAction func onHappyPanGesture(_ sender: UIPanGestureRecognizer) {
+        let point = sender.location(in: view)
+        
+        if sender.state == UIGestureRecognizerState.began {
+            // Gesture recognizers know the view they are attached to
+            let imageView = sender.view as! UIImageView
+            
+            // Create a new image view that has the same image as the one currently panning
+            newlyCreatedFace = UIImageView(image: imageView.image)
+            
+            // Add the new face to the tray's parent view.
+            view.addSubview(newlyCreatedFace)
+            
+            // Initialize the position of the new face.
+            newlyCreatedFace.center = imageView.center
+            
+            // Since the original face is in the tray, but the new face is in the
+            // main view, you have to offset the coordinates
+            newlyCreatedFace.center.y += trayView.frame.origin.y
+        } else if sender.state == UIGestureRecognizerState.changed {
+            print("Gesture changed at: \(point)")
+            happyImageView.center = CGPoint(x: point.x, y: point.y - trayView.frame.origin.y)
+        } else if sender.state == UIGestureRecognizerState.ended {
+            
+        }
     }
     
     @IBAction func onSadPanGesture(_ sender: UIPanGestureRecognizer) {
+        let point = sender.location(in: view)
+        
+        if sender.state == UIGestureRecognizerState.began {
+            // Gesture recognizers know the view they are attached to
+            let imageView = sender.view as! UIImageView
+            
+            // Create a new image view that has the same image as the one currently panning
+            newlyCreatedFace = UIImageView(image: imageView.image)
+            
+            // Add the new face to the tray's parent view.
+            view.addSubview(newlyCreatedFace)
+            
+            // Initialize the position of the new face.
+            newlyCreatedFace.center = imageView.center
+            
+            // Since the original face is in the tray, but the new face is in the
+            // main view, you have to offset the coordinates
+            newlyCreatedFace.center.y += trayView.frame.origin.y
+        } else if sender.state == UIGestureRecognizerState.changed {
+            print("Gesture changed at: \(point)")
+            sadImageView.center = CGPoint(x: point.x, y: point.y - trayView.frame.origin.y)
+        } else if sender.state == UIGestureRecognizerState.ended {
+            
+        }
     }
     
     @IBAction func onTonguePanGesture(_ sender: UIPanGestureRecognizer) {
+        let point = sender.location(in: view)
+        
+        if sender.state == UIGestureRecognizerState.began {
+            // Gesture recognizers know the view they are attached to
+            let imageView = sender.view as! UIImageView
+            
+            // Create a new image view that has the same image as the one currently panning
+            newlyCreatedFace = UIImageView(image: imageView.image)
+            
+            // Add the new face to the tray's parent view.
+            view.addSubview(newlyCreatedFace)
+            
+            // Initialize the position of the new face.
+            newlyCreatedFace.center = imageView.center
+            
+            // Since the original face is in the tray, but the new face is in the
+            // main view, you have to offset the coordinates
+            newlyCreatedFace.center.y += trayView.frame.origin.y
+        } else if sender.state == UIGestureRecognizerState.changed {
+            print("Gesture changed at: \(point)")
+            tongueImageView.center = CGPoint(x: point.x, y: point.y - trayView.frame.origin.y)
+        } else if sender.state == UIGestureRecognizerState.ended {
+            
+        }
     }
     
     @IBAction func onWinkPanGesture(_ sender: UIPanGestureRecognizer) {
+        let point = sender.location(in: view)
+        
+        if sender.state == UIGestureRecognizerState.began {
+            // Gesture recognizers know the view they are attached to
+            let imageView = sender.view as! UIImageView
+            
+            // Create a new image view that has the same image as the one currently panning
+            newlyCreatedFace = UIImageView(image: imageView.image)
+            
+            // Add the new face to the tray's parent view.
+            view.addSubview(newlyCreatedFace)
+            
+            // Initialize the position of the new face.
+            newlyCreatedFace.center = imageView.center
+            
+            // Since the original face is in the tray, but the new face is in the
+            // main view, you have to offset the coordinates
+            newlyCreatedFace.center.y += trayView.frame.origin.y
+        } else if sender.state == UIGestureRecognizerState.changed {
+            print("Gesture changed at: \(point)")
+            winkImageView.center = CGPoint(x: point.x, y: point.y - trayView.frame.origin.y)
+        } else if sender.state == UIGestureRecognizerState.ended {
+            
+        }
     }
     
 }
