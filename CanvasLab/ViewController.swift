4c7d4524d2707db07bdd24593d61fb706be292d0
wassupnari <wassupnari@gmail.com>
2016-11-03 18:40:31 -0700

    Added pan gesture to the trayView

--- CanvasLab/ViewController.swift
+++ CanvasLab/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var trayView: UIView!
+    
+    var trayOriginalCenter: CGPoint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTrayPanGesture(_ sender: UIPanGestureRecognizer) {
+        // Absolute (x,y) coordinates in parent view (parentView should be
+        // the parent view of the tray)
+        let point = sender.location(in: view)
+        let translation = sender.translation(in: view)
+        
+        if sender.state == UIGestureRecognizerState.began {
+            print("Gesture began at: \(point)")
+            trayOriginalCenter = point
+        } else if sender.state == UIGestureRecognizerState.changed {
+            print("Gesture changed at: \(point)")
+            trayView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + translation.y)
+        } else if sender.state == UIGestureRecognizerState.ended {
+            print("Gesture ended at: \(point)")
+        }
+    }
+    
 
 }
 
