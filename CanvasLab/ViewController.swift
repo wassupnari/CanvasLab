b591cea8af4ab964402a07efdb784e2cbca50e4c
wassupnari <wassupnari@gmail.com>
2016-11-03 19:40:18 -0700

    Added open and close animation

--- CanvasLab/ViewController.swift
+++ CanvasLab/ViewController.swift
@@ -13,10 +13,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var trayView: UIView!
     
     var trayOriginalCenter: CGPoint!
+    var trayCenterWhenOpen: CGPoint!
+    var trayCenterWhenClosed: CGPoint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let screenSize: CGRect = UIScreen.main.bounds
+        let screenWidth = screenSize.width
+        let screenHeight = screenSize.height
+        
+        print("screen height : \(screenHeight)")
+        print("frame height : \(self.view.frame.size.height)")
+        
+        trayCenterWhenOpen = CGPoint(x: self.view.frame.size.width/2, y: (screenHeight - 295))
+        trayCenterWhenClosed = CGPoint(x: self.view.frame.size.width/2, y: self.view.frame.size.height + 30)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,15 +42,25 @@ class ViewController: UIViewController {
         // the parent view of the tray)
         let point = sender.location(in: view)
         let translation = sender.translation(in: view)
+        let velocity = sender.velocity(in: view)
         
         if sender.state == UIGestureRecognizerState.began {
             print("Gesture began at: \(point)")
-            trayOriginalCenter = point
+            trayOriginalCenter = CGPoint(x: self.view.frame.size.width/2, y: point.y)
         } else if sender.state == UIGestureRecognizerState.changed {
             print("Gesture changed at: \(point)")
             trayView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + translation.y)
         } else if sender.state == UIGestureRecognizerState.ended {
             print("Gesture ended at: \(point)")
+            
+            UIView.animate(withDuration: 0.3, animations: {
+                if velocity.y > 0 { // moving down
+                    self.trayView.center = self.trayCenterWhenClosed
+                } else { // moving up
+                    self.trayView.center = self.trayCenterWhenOpen
+                }
+                self.view.layoutIfNeeded()
+            })
         }
     }
     
